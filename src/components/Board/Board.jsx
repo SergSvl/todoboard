a8504762309d88e9043fe7c1a3d3898c947a26c4
Sergey <srgsvl@list.ru>
2022-08-22 18:03:22 +0300

    Added focus into input on board title by mouse click

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Card from '@/components/Card';
 import Delete from '@/assets/icons/close.svg';
@@ -13,6 +13,7 @@ export const Board = ({ board, deleteBoard, changeBoards }) => {
   const { boards } = useSelector((state) => state.main);
   const { onDragStart, onDragOver, onDragLeave, onDragEnd, onDrop } = useDraggable();
   const dispatch = useDispatch();
+  const titleInputRef = useRef();
 
   const clickOutHandler = () => {
     setIsConfirmOpenned(false);
@@ -35,13 +36,21 @@ export const Board = ({ board, deleteBoard, changeBoards }) => {
 
   const onEditTitleHandler = (e) => {
     e.stopPropagation();
+    console.log("onEditTitleHandler e:", e);
     setIsEditTitle(true);
+    
   }
 
   const onChangeTitleHandler = (title) => {
     console.log("onChangeTitleHandler::", title);
     setNewTitle(title);
   }
+
+  useEffect(() => {
+    if (isEditTitle) {
+      titleInputRef.current.focus();
+    } 
+  }, [isEditTitle]);
 
   useEffect(() => {
     setNewTitle(board.title);
@@ -71,19 +80,21 @@ export const Board = ({ board, deleteBoard, changeBoards }) => {
           <div className='flex justify-beetwen border-b mb-2'>
             {isEditTitle ? (
               <input className='w-full border rounded-md h-8 px-2 mb-0'
+                ref={titleInputRef}
                 value={newTitle}
                 onChange={(e) => onChangeTitleHandler(e.target.value)}
                 onClick={(e) => e.stopPropagation()}
               />
             ) : (
               <div className='w-full h-8 text-left pt-1 pl-2 font-semibold'
-                onDoubleClick={(e) => onEditTitleHandler(e)}
+                onClick={(e) => onEditTitleHandler(e)}
               >{board.title}</div>
             )}
-            <div className='h-7 hover:cursor-pointer px-2 -border m-auto pt-1' 
+            <div className='h-7 hover:cursor-pointer px-2 -border m-auto pt-2' 
                title='Удалить группу'
+               onClick={(e) => onDeleteHandler(e)}
             >
-              <img src={Delete} className="" width="16" alt="delete" onClick={(e) => onDeleteHandler(e)} />
+              <img src={Delete} className="" width="12" alt="delete" />
             </div>
           </div>
 
